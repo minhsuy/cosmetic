0a5fec4de049d2e8d0f2c55268b94ea679e9072c
minhsuy <thaiminh0612@gmail.com>
2026-01-14 16:01:37 +0700

    feat : add booking models

--- backend/src/models/booking.model.ts
+++ backend/src/models/booking.model.ts
@@ -1,99 +1,56 @@
-// import { Schema, model, Types } from 'mongoose'
-// import { BOOKING_STATUS, BookingStatus } from '~/constant/const'
+import { Schema, model, Types } from 'mongoose'
+import { BOOKING_STATUS, BookingStatus } from '~/constant/const'
 
-// export interface IBooking {
-//   _id: Types.ObjectId
+export interface IBooking {
+  _id: Types.ObjectId
 
-//   staffId: Types.ObjectId
-//   userId?: Types.ObjectId | null
-//   guestInfoId?: Types.ObjectId | null
+  staffId: Types.ObjectId
 
-//   serviceId: Types.ObjectId
-//   scheduleId?: Types.ObjectId | null
+  userId?: Types.ObjectId | null
+  guestInfoId?: Types.ObjectId | null
 
-//   note?: string
+  serviceId: Types.ObjectId
 
-//   startTime: Date
-//   endTime: Date
+  startTime: Date
+  endTime: Date
 
-//   status: BookingStatus
+  status: BookingStatus
 
-//   createdById?: Types.ObjectId
-//   updatedById?: Types.ObjectId
-//   deletedById?: Types.ObjectId
-//   isDeleted: boolean
+  note?: string
 
-//   createdAt: Date
-//   updatedAt: Date
-// }
+  createdById?: Types.ObjectId
+  updatedById?: Types.ObjectId
+  deletedById?: Types.ObjectId
+  isDeleted: boolean
 
-// const BookingSchema = new Schema<IBooking>(
-//   {
-//     staffId: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+  createdAt: Date
+  updatedAt: Date
+}
 
-//     userId: { type: Schema.Types.ObjectId, ref: 'User', default: null, index: true },
-//     guestInfoId: { type: Schema.Types.ObjectId, ref: 'GuestInfo', default: null, index: true },
+const BookingSchema = new Schema<IBooking>(
+  {
+    staffId: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
 
-//     serviceId: { type: Schema.Types.ObjectId, ref: 'Service', required: true, index: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'User', default: null, index: true },
+    guestInfoId: { type: Schema.Types.ObjectId, ref: 'GuestInfo', default: null, index: true },
 
-//     scheduleId: { type: Schema.Types.ObjectId, ref: 'Schedule', default: null },
+    serviceId: { type: Schema.Types.ObjectId, ref: 'Service', required: true, index: true },
 
-//     note: { type: String, trim: true, default: '' },
+    startTime: { type: Date, required: true, index: true },
+    endTime: { type: Date, required: true, index: true },
 
-//     startTime: { type: Date, required: true, index: true },
-//     endTime: { type: Date, required: true, index: true },
+    status: { type: String, enum: BOOKING_STATUS, default: 'PENDING', index: true },
 
-//     status: { type: String, enum: BOOKING_STATUS, default: 'PENDING', index: true },
+    note: { type: String, trim: true, default: '' },
 
-//     createdById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
-//     updatedById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
-//     deletedById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
-//     isDeleted: { type: Boolean, default: false, index: true }
-//   },
-//   {
-//     collection: 'bookings',
-//     versionKey: false,
-//     timestamps: true
-//   }
-// )
+    createdById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+    updatedById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+    deletedById: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+    isDeleted: { type: Boolean, default: false, index: true }
+  },
+  {
+    timestamps: true
+  }
+)
 
-// /**
-//  * Rule 1: Booking phải thuộc về đúng 1 trong 2: userId hoặc guestInfoId
-//  * (khách có account) XOR (khách vãng lai)
-//  */
-// BookingSchema.pre('validate', function (next) {
-//   const hasUser = !!this.get('userId')
-//   const hasGuest = !!this.get('guestInfoId')
-//   if (hasUser === hasGuest) {
-//     return next(new Error('Booking must have exactly one of userId or guestInfoId.'))
-//   }
-//   next()
-// })
-
-// /**
-//  * Rule 2: endTime > startTime
-//  */
-// BookingSchema.pre('validate', function (next) {
-//   const start = this.get('startTime')
-//   const end = this.get('endTime')
-//   if (start && end && new Date(end).getTime() <= new Date(start).getTime()) {
-//     return next(new Error('endTime must be greater than startTime.'))
-//   }
-//   next()
-// })
-
-// // Index hay dùng để query theo staff và khung giờ
-// BookingSchema.index({ staffId: 1, startTime: 1, endTime: 1 })
-// BookingSchema.index({ serviceId: 1, startTime: 1 })
-// BookingSchema.index({ status: 1, isDeleted: 1 })
-
-// // Gợi ý check trùng lịch (làm ở service layer khi create/update):
-// // BookingModel.findOne({
-// //   staffId,
-// //   isDeleted: false,
-// //   status: { $in: ["PENDING", "CONFIRMED"] },
-// //   startTime: { $lt: endTime },
-// //   endTime: { $gt: startTime },
-// // });
-
-// export const BookingModel = model<IBooking>('Booking', BookingSchema)
+export const BookingModel = model<IBooking>('Booking', BookingSchema)
